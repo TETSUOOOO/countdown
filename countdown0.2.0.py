f12b05f1bfcc77b2d99704098004531d09accaed
Thomas Vilela <thomasavilela@gmail.com>
2017-05-01 17:06:13 +0000

    First commit!

---
+++ countdown0.2.0.py
@@ -0,0 +1,222 @@
+#! /usr/bin/python3
+# LINUXPLORE - A game that demonstrates linux command-line knowledge!
+
+import time, math, sys, os, pprint
+from subprocess import call
+
+# Globals
+
+prompt = ['You are the sole remaining survivor on an abandoned space vessel after being attacked by beligerent Windows Users...', 'You have made haste to the nearest terminal that dismantles an escape craft...', 'The only catch is that you must maneuver through murky linux commands to find the unlocking mechanism!', 'You must use the Bourne-Again Shell (bash) and conjure any inkling of knowledge in order to proceed to the escape craft!', 'Good luck or good day, space cadet!\n']
+prompt_two = 'You have one minute before the atmosphere leaks into the vessel!'
+users_list = {'Username': '', 'Points': 0}
+record = []
+response = ''
+users = []
+
+# TODO: factor negative points upon answering challenge question incorrectly
+# TODO: refactor score function
+# TODO: Only display user score at end of game
+# TODO: fix while loop for game module to properly exit game and utilize 'scoreDisplay(score)' in main
+# TODO: Read and write to a file to save user name and score
+
+
+def elapsedTime(u):
+
+    secondMinute = []
+    elap = time.time() - u
+    remain = 60 - round(elap)
+    min_remain = remain / 60
+    m = math.floor(min_remain)
+    s = remain - (m * 60)
+    secondMinute.append(m)
+    secondMinute.append(s)
+    if remain <= 0:
+        print('Time has run out!\nGame Over!')
+        sys.exit()
+    print('\nYour remaining time is {0} minutes and {1} seconds'.format(secondMinute[0], secondMinute[1]))
+    new_points = 'Points'
+    record.append(new_points)
+    score = userUpdate(users_list, record)
+    scoreDisplay(score)
+           
+def userInfo():
+    users_list['Username'] = input('\nType Username> ')    
+    print('Greetings, ' + str(users_list['Username']) + '!')
+    print('Your score is ' + str(users_list['Points']))
+    
+#NOTE: Character cycle only works in Python IDLE Shell - it's a bug!
+
+    for i in prompt:
+        for char in i:
+            print(char, end='')
+            time.sleep(0.05)
+        time.sleep(4)
+        print('\n')
+
+def userUpdate(player, factor):
+    tmp = player
+    for i in factor:
+        tmp.setdefault(i, 0)
+        tmp[i] += 1
+    return tmp
+   
+def scoreDisplay(userScore):
+    print('Score:'.center(40, '~'))
+    for k, v in userScore.items():
+        userScore.setdefault(k, 0)
+        print(str(k) + ' ' + str(v))
+    
+def userContinue(r):
+    print('You are out of tries! Continue? (y/n)')
+    r = input()
+    if r == 'y':
+        userAwait()
+        countdownGame()
+    elif r == 'n':
+        print('Thank you for playing!')
+        sys.exit()
+
+def userAwait():
+    ready = input('Are you ready? (y/n) or \'q\' for quit\n')
+    if ready == 'n':
+        print('No problem...')
+        userAwait()
+    elif ready == 'y':
+        for j in range(0, 3):
+            print('. ', end='')
+            time.sleep(0.5)
+        countdownGame()
+    elif ready == 'q':
+        print('Okay, goodbye ' + str(users_list['Username']) + '\n')
+        sys.exit()
+               
+# Game module
+def countdownGame():
+    active = True
+    response = ''
+    start = time.time()
+    print(prompt_two)
+    turn = 3
+    challenge = ''
+    while turn > 0:
+        while active == True:
+            print('Which choice selects the \'Print Working Directory\'?')
+            print('A).    cd')
+            print('B).    ls')
+            print('C).    pwd')
+            challenge = input()
+            if challenge.lower() == 'c':
+                print('Success! You have entered \'pwd\' for \'print working directory\'!\n')
+                call(["pwd"])
+                elapsedTime(start)
+            else:
+                turn -= 1
+                if turn == 0:
+                    userContinue(response)
+                else:
+                    if turn == 1:
+                        print('Please try again. You have ' + str(turn) + ' try remaining!\n')
+                        elapsedTime(start)
+                        continue
+                    else:
+                        print('Please try again. You have ' + str(turn) + ' tries remaining!\n')
+                        elapsedTime(start)
+                        continue 
+        
+            print('Which command allows the user to view the amount of used memory on the system?')
+            print('A).    df')
+            print('B).    free')
+            print('C).    apropos')
+            challenge = input()
+            if challenge.lower() == 'b':
+                call(["free"])
+                print('Hey, you did it, space cadet... that sounded diminutive. SORRY.\n')
+                elapsedTime(start)
+            else:
+                turn -= 1
+                if turn == 0:
+                    userContinue(response)
+                else:
+                    if turn == 1:
+                        print('Please try again. You have ' + str(turn) + ' try remaining!\n')
+                        elapsedTime(start)
+                        continue
+                    else:
+                        print('Please try again.\nYou have ' + str(turn) + ' tries remaining!\n')
+                        elapsedTime(start)
+                        continue    
+
+            print('Which option allows the user to view extra details concerning the list of contents within the working directory?')
+            print('A).    -h')
+            print('B).    -i')
+            print('C).    -l')
+            challenge = input()
+            if challenge.lower() == 'c':
+                call(["ls", "-l"])
+                print('I see what you did there, and it\'s amazing!')
+                elapsedTime(start)
+            else:
+                turn -= 1
+                if turn == 0:
+                    userContinue(response)
+                else:
+                    if turn == 1:
+                        print('Please try again. You have ' + str(turn) + ' try remaining!\n')
+                        elapsedTime(start)
+                        continue
+                    else:
+                        print('Please try again.\nYou have ' + str(turn) + ' tries remaining!\n')
+                        elapsedTime(start)
+                        continue
+                    
+            print('How would you access the root directory?')
+            print('A).    cd root/')
+            print('B).    cd ./')
+            print('C).    cd ~/')
+            challenge = input()
+            if challenge.lower() == 'c':
+                print('BINGO was his name-o!')
+                elapsedTime(start)
+            else:
+                turn -= 1
+                if turn == 0:
+                    userContinue(response)
+                else:
+                    if turn == 1:
+                        print('Please try again. You have ' + str(turn) + ' try remaining!\n')
+                        elapsedTime(start)
+                        continue
+                    else:
+                        print('Please try again. You have ' + str(turn) + ' tries remaining!\n')
+                        elapsedTime(start)
+                        continue
+            active = False
+        turn = 0
+    print('You win!')
+    for x in userList.users:
+        print(x)
+   
+if __name__ == '__main__':
+    
+    print('Welcome to \'LINUXPLORE\''.center(40, '~') + '\n')
+    userInfo()
+    userAwait()
+    countdownGame()
+   
+    
+    
+  
+    
+
+    
+        
+            
+            
+            
+                        
+
+
+
+
+
+
